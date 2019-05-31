8de5e028504c8ef52d64af21597fe9d9c5825355
edj19 <edj17@lzu.edu.cn>
2019-05-31 15:52:52 +0800

    String Basic

---
+++ Array_and_String/String/Basic_extra_operations_02.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+
+using namespace std;
+
+
+#include <iostream>
+
+int main() {
+    string s1 = "Hello World";
+    // 1. concatenate
+    s1 += "!";
+    cout << s1 << endl;
+    // 2. find
+    cout << "The position of first 'o' is: " << s1.find('o') << endl;
+    cout << "The position of last 'o' is: " << s1.rfind('o') << endl;
+    // 3. get substr
+    cout << s1.substr(6, 5) << endl;
+    return 0;
+}
---
+++ Array_and_String/String/Basic_mutable_string_01.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+
+using namespace std;
+
+
+int main() {
+    string s1 = "Hello World";
+    s1[5] = ',';
+    cout << s1 << endl;
+    return 0;
+}
